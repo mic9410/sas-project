9afd9d200affd1b31da212bc73a46f9aebac704f
Foryt Michal-TGQ647 <Michal.Foryt@motorolasolutions.com>
2019-01-29 09:45:59 +0100

    Images works <3

--- src/components/OverallLayer.vue
+++ src/components/OverallLayer.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div><h4 class="col-lg-8">{{overallLayerText}}</h4></div>
+    <div><h4 id="test-anchor" class="col-lg-8">{{overallLayerText}}</h4></div>
     <div>
-     <img v-bind:src="vinIlosciowyPath" v-bind:alt="this.text1"/>
+     <img :src="testSrc" alt="this.text1"/>
     </div>
   </div>
 </template>
@@ -12,9 +12,11 @@ export default {
   name: 'overall-layer',
   data () {
     return {
+      testSrc: require('../assets/logo.png'),
       overallLayerText: 'W warstwie całościowej przyjrzymy się całościowej analizie vintage, ' +
         'bez podziału na zmienne, grupy, czy też możliwe okresy vin.',
-      vinIlosciowyPath: '.data/1_all_act_act_age.PNG',
+      /* vinIlosciowyPath: '.data/1_all_act_act_age.PNG', */
+      vinIlosciowyPath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvzkUp5wqiicRLX4KjzbO5pllFPxyWjxiBuwN-x1fSa53zbhTq',
       vinIlosciowyAsPath: 'D:\\Repos\\sas-raport\\data\\vin_ilosciowy_as.PNG',
       vinKwotowyPath: 'D:\\Repos\\sas-raport\\data\\vin_kwotowy.PNG',
       vinKwotowyAsPath: 'D:\\Repos\\sas-raport\\data\\vin_kwotowy_as.PNG',
