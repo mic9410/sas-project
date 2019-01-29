2cd66948206408b3ce702db5d10319139f111958
mic9410 <mic132@gazeta.pl>
2019-01-29 20:12:18 +0100

    Connecting host and url works.

--- src/components/OverallLayer.vue
+++ src/components/OverallLayer.vue
@@ -2,7 +2,7 @@
   <div>
     <div><h4 id="test-anchor" class="col-lg-8">{{overallLayerText}}</h4></div>
     <div>
-     <img :src="testSrc" alt="this.text1"/>
+      <img v-bind:src="this.getImgUrl(this.host , this.file)" alt="this.text1"/>
     </div>
   </div>
 </template>
@@ -12,7 +12,9 @@ export default {
   name: 'overall-layer',
   data () {
     return {
-      testSrc: require('../assets/logo.png'),
+      host: 'https://web.sgh.waw.pl/~gkoloch',
+      file: '/koloch_talk.png',
+      full: this.host + this.file,
       overallLayerText: 'W warstwie całościowej przyjrzymy się całościowej analizie vintage, ' +
         'bez podziału na zmienne, grupy, czy też możliwe okresy vin.',
       /* vinIlosciowyPath: '.data/1_all_act_act_age.PNG', */
@@ -21,6 +23,11 @@ export default {
       vinKwotowyPath: 'D:\\Repos\\sas-raport\\data\\vin_kwotowy.PNG',
       vinKwotowyAsPath: 'D:\\Repos\\sas-raport\\data\\vin_kwotowy_as.PNG',
       text1: 'dupa1'
+    }
+  },
+  methods: {
+    getImgUrl (hostVar, fileVar) {
+      return (hostVar + fileVar).toString()
     }
   }
 }
