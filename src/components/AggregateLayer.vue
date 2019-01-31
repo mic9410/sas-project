d2aef51969b49a8859535975591b27918bc9a467
mic9410 <mic132@gazeta.pl>
2019-01-31 02:04:44 +0100

    Aggregate Layer in progress

--- src/components/AggregateLayer.vue
+++ src/components/AggregateLayer.vue
@@ -1,14 +1,49 @@
 <template>
-  <div><h4 class="col-lg-8">{{aggregateLayerText}}</h4></div>
+  <div>
+    <div><h4 id="test-anchor" class="col-lg-8">{{overallLayerText}}</h4></div>
+    <div class="container select-params col-md-8 align-content-center">
+      <form>
+        <div class="form-group row">
+          <label for="due" class="col-md-6 col-form-label">Określ due (minimalna liczba opóźnionych rat) </label>
+          <select class="form-control col-md-2 btn btn-secondary " id="due" v-model="dueAL">
+            <option selected>1</option>
+            <option>2</option>
+            <option>3</option>
+          </select>
+        </div>
+        <div class="form-group row">
+          <label for="wallet-type" class="col-md-6 col-form-label">Typ protfela (kredyt gotówkowy czy raty
+            produktu) </label>
+          <select class="form-control col-md-2 btn btn-secondary" id="wallet-type" v-model="walletTypeAL">
+            <option selected>all</option>
+            <option>css</option>
+            <option>ins</option>
+          </select>
+        </div>
+        {{this.getImgUrl(this.hostAL, this.dueAL, 'vin_a' , this.walletTypeAL)}}
+      </form>
+      <div class="row align-items-center justify-content-center">
+        <button v-on:click="displayResults" class="btn btn-info showGraph">Pokaż wykres</button>
+      </div>
+      <img v-bind:src="this.getImgUrl(this.hostAL, this.dueAL, 'vin_a' , this.walletTypeAL)" alt=""/>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'aggregate-layer',
+  name: 'overall-layer',
   data () {
     return {
-      aggregateLayerText: 'Warstwa zbiorcza dotyczy analizy vintage kwotowego i ilościowego dla vin=1, 2 oraz 3, ' +
-        'a także z podziałem na kredyty gotówkowe i ratalne.'
+      hostAL: 'https://s3.amazonaws.com/projektsas/produkty_projektu/',
+      dueAL: '1',
+      walletTypeAL: 'all',
+      overallLayerText: 'W warstwie zbiorczej zajmujemy się '
+    }
+  },
+  methods: {
+    getImgUrl (host, due, vinType, wallet) {
+      return (host + due + '/' + wallet + '/' + vinType + '/Vin.png').toString()
     }
   }
 }
